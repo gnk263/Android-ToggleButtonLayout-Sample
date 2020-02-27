3259c12b512f4c8f41530c32b0eef92e2ea30a91
gnk263 <gnk263@users.noreply.github.com>
2020-02-27 10:32:30 +0900

    動的なトグルボタンを追加

--- app/src/main/java/jp/test/togglebuttonsample/MainActivity.kt
+++ app/src/main/java/jp/test/togglebuttonsample/MainActivity.kt
@@ -1,12 +1,19 @@
 package jp.test.togglebuttonsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.savvyapps.togglebuttonlayout.Toggle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        repeat(3) {
+            val toggle = Toggle(1 + it, null, "test$it")
+            toggle_button_layout1.addToggle(toggle)
+        }
     }
 }
